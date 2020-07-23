77e9018c578d4b9ed6136314fdf9e1cf3b142611
Pavel <pogosovpavelpoker@yandex.ru>
2020-07-23 20:04:35 +0300

    Updates

---
+++ precache-manifest.cfca59c786a129d1c57ccb50e2efeab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6e0c8ef2d1bef29f7338bbca029880",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "32fba9643376dbfdaaae",
+    "url": "/TodoList/static/css/main.8af758ff.chunk.css"
+  },
+  {
+    "revision": "d55bc6414e2c722d0a09",
+    "url": "/TodoList/static/js/2.7723b6e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoList/static/js/2.7723b6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fba9643376dbfdaaae",
+    "url": "/TodoList/static/js/main.d47ce1df.chunk.js"
+  },
+  {
+    "revision": "588c4c424406449bcff3",
+    "url": "/TodoList/static/js/runtime-main.2f48f307.js"
+  }
+]);
